d777242c7d9f21b33560d1f7600aae1ebf1aee98
tyler finley & bunker <student@epicodus.com>
2015-09-02 14:14:41 -0700

    refactor app.rb file

--- lib/habit.rb
+++ lib/habit.rb
@@ -1,5 +1,13 @@
 class Habit < ActiveRecord::Base
   has_many :milestones
   has_many :users, through: :milestones
+  validates(:name, {:presence => true, :length => { minimum: 1 }})
+  before_save(:capitalize_habit)
 
+private
+
+  def capitalize_habit
+    self.name=(name.titleize)
+  end
+  
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
 
   has_many :milestones
   has_many :habits, through: :milestones
+  validates(:name, {:presence => true, :length => { minimum: 1 }})
+  before_save(:capitalize_user)
+
+private
+
+  def capitalize_user
+    self.name=(name.titleize)
+  end
+
 end
