45b159905ef5848a410d7b8b009152ab2ba7fdc0
tyler finley & travis crampton <student@epicodus.com>
2015-09-03 11:21:47 -0700

    fix the login page

--- app.rb
+++ app.rb
@@ -258,8 +258,9 @@ end
 post '/messages/new' do
   habit_id= params.fetch("id")
   @habit = Habit.find(habit_id)
+  title = params.fetch('title')
   message = params.fetch('message')
-  @message = Messageboard.create({:message => message, :habit_id => habit_id})
+  @message = Messageboard.create({:message => message, :title => title, :habit_id => habit_id})
   @messageboards = Messageboard.all()
 
   erb :habit_detail
